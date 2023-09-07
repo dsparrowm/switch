cc0107246e14acf3cfe22853f929442e1643c350
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-09-07 11:50:24 +0100

    added a test file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log('Hello from server');
     res.status(200);
     res.json({
         message: "Hello world"
