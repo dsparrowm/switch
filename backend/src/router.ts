8e607842e18c18152867c619c1b809845b2cc6e0
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-09-18 17:34:34 +0100

    added routes to create new private messages between 2 users

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -258,7 +258,8 @@ router.delete('/organisations/:id', () => {})
  * MESSAGES To be implemented later
  */
 router.get('/messages/private', async (req, res) => {
-    const {senderId, receiverId} = req.body;
+    const receiverId = parseInt(req.query.receiverId);
+    const senderId = parseInt(req.query.senderId);
     try {
         const messages = await prisma.message.findMany({
             where: {
@@ -289,19 +290,34 @@ router.get('/messages/private', async (req, res) => {
 })
 
 router.get('/messages/group', async (req, res) => {
-    const {departmentId} = req.body;
+    const departmentId = parseInt(req.query.departmentId);
     try {
         const messages = await prisma.message.findMany({
             where: {
                 departmentId: departmentId
             },
-            orderBy: {
-                createdAt: 'asc'}
+            orderBy: {createdAt: 'asc'}
         });
         res.status(200).json({messages})
     } catch (err) {
         res.status(404).json({message: err.message})
     } 
+})
+
+router.post('/messages/new', async (req, res) => {
+    const {senderId, recipientId, content} = req.query;
+    try {
+        const message = await prisma.message.create({
+            data: {
+                senderId,
+                recipientId,
+                content,
+            }
+        })
+        res.status(200).json({message})
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
 })
 
 /**
