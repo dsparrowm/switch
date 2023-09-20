c78d1e48788fda8418c6affe74ac7a46875c86e4
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-09-20 15:55:04 +0100

    fixed cors issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,41 @@ dotenv.config()
 import app from './server'
 import http from 'http';
 import { Server} from 'socket.io';
+import prisma from './db';
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {cors: {
+    origin: '*',
+}});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('join-department', (departmentId) => {
+        socket.join(departmentId);  
+    })
+
+        socket.on('groupMessage', async (data) => {
+            try {
+                const savedMessage = await prisma.message.create({
+                    data: {
+                        content: data.content,
+                        senderId: data.senderId,
+                        departmentId: data.departmentId
+                    }
+                })
+
+                io.to(data.departmentId).emit('groupMessage', savedMessage)
+
+            } catch (err) {
+                console.log(err);
+            }
+            
+        })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
 })
 
 
